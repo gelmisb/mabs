f2e56794a7111d1c76b0346796131468e09561d1
gelmisb@gmail.com <gelmisb@gmail.com>
2018-06-27 17:31:00 +0100

    Addtional files

---
+++ app/src/main/java/b00080902/mabs2/ClickListener.java
@@ -0,0 +1,13 @@
+package b00080902.mabs2;
+
+import android.view.View;
+
+/**
+ * Created by VMac on 06/01/17.
+ */
+
+public interface ClickListener {
+    void onClick(View view, int position);
+
+    void onLongClick(View view, int position);
+}
